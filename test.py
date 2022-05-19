cf06d7669735f02b6ef76d338c321e49137b88a7
Tri Doan <tridoan1998@gmail.com>
2022-05-19 11:57:33 -0700

    test new branch in git

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 #write code to multiply a number by 7 using * operator
 def main():
-    print("test")
+    multiple_num_by_7(7)
 
+def multiple_num_by_7(number: int) -> None:
+    print(number*7)
+
+def test() -> None:
+    return
+    
 if __name__ == "__main__":
     main()
 
